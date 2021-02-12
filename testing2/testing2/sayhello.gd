864da41272c02ed7d1984907305eae4b4690e7ec
dvriesin <78976161+dvriesin@users.noreply.github.com>
2021-02-12 14:18:10 -0500

    Add files via upload

---
+++ testing2/testing2/sayhello.gd
@@ -0,0 +1,44 @@
+extends Panel
+
+var radius = 150
+var circleP = Vector2(200,300)
+var myP: PoolVector2Array = [circleP, circleP + Vector2(radius,0)]
+var sineHeight:float
+var roundAngle:int =0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	get_node("Radius").points = myP
+	get_node("CoseLine").points = myP
+	get_node("SineLine").points = myP
+	
+var theta = 0.0
+func _process(delta):
+	theta += delta
+	roundAngle = int(theta*180/PI)
+	if roundAngle > 360:
+		roundAngle -= 360
+		theta -= 2*PI
+	if roundAngle < 0:
+		roundAngle += 360
+		theta += 2*PI
+	print(roundAngle)
+	get_node("Radius").points[1] = Vector2(radius*cos(theta), radius*sin(theta)) + circleP 
+	get_node("CoseLine").points[1] = myP[0] + Vector2(radius*cos(theta), 0)
+	get_node("SineLine").points[0] = get_node("CoseLine").points[1] 
+	get_node("SineLine").points[1] = get_node("SineLine").points[0] + Vector2(0,radius*sin(theta))
+	update()
+	
+#drawing sine lines 
+func _draw():
+	var startHeight:Vector2 = Vector2(400,200)
+	var sineVec:Vector2 = get_node("SineLine").points[1] - get_node("SineLine").points[0]
+	var endHeightLength:float = sineVec.length()*sineVec.sign().y
+	var endHeight:Vector2 = startHeight + Vector2(0, endHeightLength)
+	draw_line(startHeight, endHeight ,Color(0.0,0.0,1.0),2.0)
+	
+
+
+
